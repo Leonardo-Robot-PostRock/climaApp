7fc4e7b6da9f66cfd54bb3306ebf99e230d2d592
Leonardo <leonardo38361@gmail.com>
2023-09-13 16:45:27 -0300

    creación de clase

---
+++ models/searches.js
@@ -0,0 +1,16 @@
+class Searches {
+	historial = ['Tegucigalpa', 'Madrid', 'San José'];
+
+	constructor() {
+		//TODO: leer db si existe
+	}
+
+	async ciudad(lugar = '') {
+		//petición http
+		console.log(lugar);
+
+		return []; // retornar los lugares que coincidan
+	}
+}
+
+module.exports = Searches;
