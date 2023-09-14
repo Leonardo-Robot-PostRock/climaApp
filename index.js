1602db9a5c8863c45ebf9005e8b39d5a1704d3aa
Leonardo <leonardo38361@gmail.com>
2023-09-14 17:18:33 -0300

    modelo para controlar la aplicacion

--- index.js
+++ index.js
@@ -4,16 +4,36 @@ const {
 	pause,
 	confirm,
 } = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
 	let opt = '';
 	let confirmOption = '';
+	const search = new Searches();
 
 	do {
 		const opt = await inquirerMenu();
 
 		switch (opt) {
 			case 1:
+				//Mostrar mensaje
+				const lugar = await readInput('Ciudad: ');
+				console.log(lugar);
+
+				//Buscar los lugares
+
+				//Seleccionar el lugar
+
+				//Clima
+
+				//Mostrar resultados
+				console.log('\nInformación de la ciudad\n'.green);
+				console.log('Ciudad:');
+				console.log('Lat:');
+				console.log('Lng:');
+				console.log('Temperatura:');
+				console.log('Mínima:');
+				console.log('Máxima:');
 				break;
 			case 2:
 				break;
