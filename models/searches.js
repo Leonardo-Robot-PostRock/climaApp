0e3e1b2aadaa4f87da75dcdf4d7f9b3054a23583
Leonardo <leonardo38361@gmail.com>
2023-09-16 01:30:25 -0300

    usando api de mapbox geolocation

--- models/searches.js
+++ models/searches.js
@@ -11,7 +11,9 @@ class Searches {
 		//petición http
 		// console.log('ciudad', lugar);
 		try {
-			const resp = await axios.get('https://reqres.in/api/users?page=2');
+			const resp = await axios.get(
+				'https://api.mapbox.com/geocoding/v5/mapbox.places/madr.json?proximity=ip&language=es&access_token=pk.eyJ1IjoibXJxd2VydHkiLCJhIjoiY2xtbDh5MzN4MDgxdTJrbndidXc4dDVqNyJ9.E_8Iwz8a3UiWzGGEDetqPg&limit=5'
+			);
 			console.log(resp.data);
 
 			return []; // retornar los lugares que coincidan
